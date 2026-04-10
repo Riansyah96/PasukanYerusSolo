10952a42ad6ff7e4c41408c5a4c594373a7c5f0c
Aby <maulidtbb@gmail.com>
2026-04-10 14:33:16 +0700

    Rute API

--- job-portal-project/backend/routes/authRoutes.js
+++ job-portal-project/backend/routes/authRoutes.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const JobController = require('../controllers/JobController');
+const ProfileController = require('../controllers/ProfileController'); // Import Baru
+const authMiddleware = require('../middleware/auth');
 
-// Routes Autentikasi
+// --- AUTH ---
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// Routes Lowongan
+// --- LOWONGAN (Tugas Anggota 2 & 3) ---
 router.get('/jobs', JobController.index);
-router.post('/jobs', JobController.store);
+router.post('/jobs', authMiddleware, JobController.store);
+router.put('/jobs/:id', authMiddleware, JobController.update);
+router.delete('/jobs/:id', authMiddleware, JobController.destroy);
+
+// --- PROFIL (Tugas Anggota 4) ---
+router.get('/profile', authMiddleware, ProfileController.show);
+router.post('/profile', authMiddleware, ProfileController.upsert);
 
 module.exports = router;
