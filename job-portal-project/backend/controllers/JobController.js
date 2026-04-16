df555bc144e7e6792be2971c5a2db557b9f6a7f1
Aby <maulidtbb@gmail.com>
2026-04-16 12:36:30 +0700

    JobController

--- job-portal-project/backend/controllers/JobController.js
+++ job-portal-project/backend/controllers/JobController.js
@@ -20,6 +20,7 @@ exports.index = async (req, res) => {
 };
 
 // controllers/JobController.js
+const pool = require('../config/db');
 
 class JobController {
   // FITUR 3: Eksplorasi Lowongan (Tugas Aby)
