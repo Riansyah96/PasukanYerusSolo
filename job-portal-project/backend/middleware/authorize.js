a8ce55b3fab1fe17b9a4dc00399e83923cd35194
Iqbal Dwi Kurniawan <iqbaldwikurniawan29@gmail.com>
2026-04-29 23:02:21 +0700

    optimasi  Middleware Authorization

--- job-portal-project/backend/middleware/authorize.js
+++ job-portal-project/backend/middleware/authorize.js
@@ -1,9 +1,10 @@
 const authorize = (role) => {
     return (req, res, next) => {
+        // req.user didapat dari middleware auth sebelumnya
         if (req.user.role !== role) {
             return res.status(403).json({ 
                 status: "fail", 
-                message: `Akses ditolak, hanya untuk role: ${role}` 
+                message: `Akses ditolak, fitur ini hanya untuk ${role}` 
             });
         }
         next();
