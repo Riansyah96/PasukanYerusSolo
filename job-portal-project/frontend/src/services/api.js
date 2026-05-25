597cba60fffd967e4de9f0c103193ec770219004
Riansyah96 <arkalbojong@gmail.com>
2026-05-25 20:54:32 +0700

    Setup Service Axios Terpusat & Base Layout

--- job-portal-project/frontend/src/services/api.js
+++ job-portal-project/frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5005/api', // Sesuaikan dengan port backend kamu
+    baseURL: 'http://localhost:5005/api',
 });
 
-// Otomatis tempelkan Token JWT jika ada di localStorage
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
